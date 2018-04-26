f48a243249fc1cb6305833e2eece8dc32dfdea5f
jcpsantiago <exalowprofile@gmail.com>
2018-04-26 10:33:45 +0200

    storage.R code clean-up

--- storage.R
+++ storage.R
@@ -1,6 +1,5 @@
 save_data_tidy <- function(con, df, table){
-  # df <- map_df(fields, ~ input[[.]])
-  
+
   if(!(is.null(df$date_of_birth))){
     df %>%
       mutate(date_of_birth = as.character(date_of_birth),
@@ -9,32 +8,6 @@ save_data_tidy <- function(con, df, table){
       db_insert_into(con, table, .)
   } else
     db_insert_into(con, table, df)
-}
-
-# save_data <- function(data, table) {
-#   # Connect to the database
-#   db <- dbConnect(SQLite(), sqlitePath)
-#   # Construct the update query by looping over the data fields
-#   query <- sprintf(
-#     "INSERT INTO %s (%s) VALUES ('%s')",
-#     table, 
-#     paste(names(data), collapse = ", "),
-#     paste(data, collapse = "', '")
-#   )
-#   # Submit the update query and disconnect
-#   dbGetQuery(db, query)
-#   dbDisconnect(db)
-# }
-
-load_data <- function(table) {
-  # Connect to the database
-  db <- dbConnect(SQLite(), sqlitePath)
-  # Construct the fetching query
-  query <- sprintf("SELECT * FROM %s", table)
-  # Submit the fetch query and disconnect
-  data <- dbGetQuery(db, query)
-  dbDisconnect(db)
-  data
 }
 
 map_study_task <- function(study, task){
@@ -65,3 +38,29 @@ map_part_study <- function(study){
   dbGetQuery(db, query)
   dbDisconnect(db)
 }
+
+# save_data <- function(data, table) {
+#   # Connect to the database
+#   db <- dbConnect(SQLite(), sqlitePath)
+#   # Construct the update query by looping over the data fields
+#   query <- sprintf(
+#     "INSERT INTO %s (%s) VALUES ('%s')",
+#     table, 
+#     paste(names(data), collapse = ", "),
+#     paste(data, collapse = "', '")
+#   )
+#   # Submit the update query and disconnect
+#   dbGetQuery(db, query)
+#   dbDisconnect(db)
+# }
+
+# load_data <- function(table) {
+#   # Connect to the database
+#   db <- dbConnect(SQLite(), sqlitePath)
+#   # Construct the fetching query
+#   query <- sprintf("SELECT * FROM %s", table)
+#   # Submit the fetch query and disconnect
+#   data <- dbGetQuery(db, query)
+#   dbDisconnect(db)
+#   data
+# }
